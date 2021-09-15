1abedaa47b4499d507edb6a190da0a8d38ce010d
Hirusha Rukmal <79465022+HirushaRukmal@users.noreply.github.com>
2021-09-16 01:42:44 +0530

    Frontend impl

---
+++ app/src/main/java/com/example/squadron_cinema/Login.java
@@ -0,0 +1,24 @@
+package com.example.squadron_cinema;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  //hiding the titlebar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();  //hiding the actionbar
+
+
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
